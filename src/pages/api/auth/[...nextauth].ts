603d6cae0d9054d173ec0fdad4d6c3f2f2ee71db
Young <yk3057@columbia.edu>
2025-04-25 17:05:13 -0400

    Initial setup

---
+++ src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,25 @@
+import NextAuth from "next-auth";
+import GitHubProvider from "next-auth/providers/github";
+import { SupabaseAdapter } from "@next-auth/supabase-adapter";
+import { createClient } from '@supabase/supabase-js';
+
+const supabase = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.SUPABASE_SERVICE_ROLE_KEY!
+);
+
+export const authOptions = {
+  providers: [
+    GitHubProvider({
+      clientId: process.env.GITHUB_ID!,
+      clientSecret: process.env.GITHUB_SECRET!,
+    }),
+  ],
+  adapter: SupabaseAdapter({
+    url: process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    secret: process.env.SUPABASE_SERVICE_ROLE_KEY!
+  }),
+  secret: process.env.NEXTAUTH_SECRET,
+};
+
+export default NextAuth(authOptions);
